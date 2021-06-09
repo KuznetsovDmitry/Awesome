c6c3baed55da29f77ea99930605d49d9fcca9be8
Dmitry.A.Kuznetsov <dmitry.a.kuznetsov@kaspersky.com>
2021-06-09 20:18:13 +0300

    Awesome Initial

---
+++ Awesome/Shared/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Shared
+//
+//  Created by Dmitry Kuznetsov on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
